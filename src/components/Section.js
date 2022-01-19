b505eff68f4a9a3985d672035eecf4bb01ecf8e6
james serez <jdserez@uwaterloo.ca>
2022-01-19 18:55:35 -0500

    updated assets

--- src/components/Section.js
+++ src/components/Section.js
@@ -7,9 +7,9 @@ const Section = ({ data }) => {
   return (
     <Link to={route} class="flex flex-row justify-between pt-2 md:pt-4 lg:pt-6 dark:bg-black dark:text-white hover:text-primary">
       <div class="flex flex-col max-w-48">
-        <h1 class="font-bold text-lg sm:text-xl lg:text-3xl my-2 w-64 xl:pb-4 lg:pb-4 md:pb-1 sm:pb-1">{title}</h1>
+        <h1 class="font-semibold text-lg sm:text-xl lg:text-3xl my-2 w-64 xl:pb-4 lg:pb-4 md:pb-1 sm:pb-1">{title}</h1>
         <div class="border border-gray-300 border-current colo w-16 md:w-20 lg:w-10"></div>
-        <p class="font-bold text-gray-500 uppercase my-1 pt-1 text-sm lg:text-md xl:pt-4 lg:pt-4 md:pt-1 sm:pt-1">{label}</p>
+        <p class="font-semibold text-gray-600 uppercase my-1 pt-1 text-sm lg:text-md xl:pt-2 lg:pt-2 md:pt-1 sm:pt-1">{label}</p>
         <p class="uppercase text-gray-500 my-1 text-sm lg:text-sm">{year}</p>
       </div>
       <div class="p-2 pb-2">
